912b3eaf80732b9860ecd0100c8faafa19362973
JavaRaf <rafaelconceicao722@gmail.com>
2024-08-23 01:48:44 -0300

    base

---
+++ src/renderer.js
@@ -0,0 +1,9 @@
+
+const inputSelect = document.getElementById('select-file'); // input file select
+const labelSelect = document.getElementById('select-file-label'); // label file select 
+
+const inputDestination = document.getElementById('destination'); // input destination
+const labelDestination = document.getElementById('label-destination'); // label destination
+
+
+
